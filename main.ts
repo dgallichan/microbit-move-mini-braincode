06d6cb8e9b2492fcb8c89dc99fa4dfbdebfd2147
Daniel Gallichan <dgallichan@yahoo.com>
2023-06-23 17:04:53 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -60,8 +60,14 @@ radio.onReceivedValueDeprecated(function (name, value) {
         }
     }
     if (name == "Go") {
-        if (value >= 0) {
+        if (value > 0) {
             pins.servoWritePin(AnalogPin.P1, 180)
+            pins.servoWritePin(AnalogPin.P2, 0)
+            basic.pause(value)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+        } else if (value < 0) {
+            pins.servoWritePin(AnalogPin.P1, 0)
             pins.servoWritePin(AnalogPin.P2, 180)
             basic.pause(value)
             pins.digitalWritePin(DigitalPin.P1, 0)
@@ -69,10 +75,16 @@ radio.onReceivedValueDeprecated(function (name, value) {
         }
     }
     if (name == "Rotate") {
-        if (value >= 0) {
+        if (value > 0) {
             pins.servoWritePin(AnalogPin.P1, 180)
-            pins.servoWritePin(AnalogPin.P2, 0)
+            pins.servoWritePin(AnalogPin.P2, 180)
             basic.pause(value)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+        } else if (value < 0) {
+            pins.servoWritePin(AnalogPin.P1, 0)
+            pins.servoWritePin(AnalogPin.P2, 0)
+            basic.pause(-1 * value)
             pins.digitalWritePin(DigitalPin.P1, 0)
             pins.digitalWritePin(DigitalPin.P2, 0)
         }
